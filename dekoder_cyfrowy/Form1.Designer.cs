408f9f9001aa22791cb0b59bc78a45f8f0515f43
Robert Paszkowski <robert.paszkowski@wat.edu.pl>
2020-05-07 20:45:18 +0200

    dodano 9 kolumn, jeden dekoder na switchu.
    to do: a co jeśli jest za krótki string?

--- dekoder_cyfrowy/Form1.Designer.cs
+++ dekoder_cyfrowy/Form1.Designer.cs
@@ -34,16 +34,28 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::dekoder_cyfrowy.Properties.Resources._0;
-            this.pictureBox1.Location = new System.Drawing.Point(335, 6);
+            this.pictureBox1.Location = new System.Drawing.Point(170, 356);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(28, 166);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -52,11 +64,11 @@
             // 
             // button0
             // 
-            this.button0.Location = new System.Drawing.Point(210, 12);
+            this.button0.Location = new System.Drawing.Point(13, 67);
             this.button0.Name = "button0";
-            this.button0.Size = new System.Drawing.Size(69, 111);
+            this.button0.Size = new System.Drawing.Size(129, 42);
             this.button0.TabIndex = 2;
-            this.button0.Text = "button0";
+            this.button0.Text = "dekoduj";
             this.button0.UseVisualStyleBackColor = true;
             this.button0.Click += new System.EventHandler(this.button0_Click);
             // 
@@ -65,18 +77,18 @@
             this.numericUpDown1.InterceptArrowKeys = false;
             this.numericUpDown1.Location = new System.Drawing.Point(13, 33);
             this.numericUpDown1.Maximum = new decimal(new int[] {
-            99999,
+            999999999,
             0,
             0,
             0});
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(84, 20);
+            this.numericUpDown1.Size = new System.Drawing.Size(129, 20);
             this.numericUpDown1.TabIndex = 1;
             // 
             // pictureBox2
             // 
             this.pictureBox2.Image = global::dekoder_cyfrowy.Properties.Resources._0;
-            this.pictureBox2.Location = new System.Drawing.Point(335, 178);
+            this.pictureBox2.Location = new System.Drawing.Point(170, 184);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(28, 166);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -86,7 +98,7 @@
             // pictureBox3
             // 
             this.pictureBox3.Image = global::dekoder_cyfrowy.Properties.Resources._0;
-            this.pictureBox3.Location = new System.Drawing.Point(335, 350);
+            this.pictureBox3.Location = new System.Drawing.Point(170, 12);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(28, 166);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -96,17 +108,84 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(33, 85);
+            this.label1.Location = new System.Drawing.Point(12, 128);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 8;
             this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::dekoder_cyfrowy.Properties.Resources._0;
+            this.pictureBox4.Location = new System.Drawing.Point(204, 356);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(28, 166);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 9;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::dekoder_cyfrowy.Properties.Resources._0;
+            this.pictureBox5.Location = new System.Drawing.Point(204, 184);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(28, 166);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 10;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::dekoder_cyfrowy.Properties.Resources._0;
+            this.pictureBox6.Location = new System.Drawing.Point(204, 12);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(28, 166);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 11;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = global::dekoder_cyfrowy.Properties.Resources._0;
+            this.pictureBox7.Location = new System.Drawing.Point(238, 356);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(28, 166);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 12;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = global::dekoder_cyfrowy.Properties.Resources._0;
+            this.pictureBox8.Location = new System.Drawing.Point(238, 184);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(28, 166);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 13;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Image = global::dekoder_cyfrowy.Properties.Resources._0;
+            this.pictureBox9.Location = new System.Drawing.Point(238, 12);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(28, 166);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox9.TabIndex = 14;
+            this.pictureBox9.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(791, 528);
+            this.ClientSize = new System.Drawing.Size(285, 528);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
@@ -114,11 +193,17 @@
             this.Controls.Add(this.button0);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "GURT";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -132,6 +217,12 @@
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
     }
 }
 
--- dekoder_cyfrowy/Form1.cs
+++ dekoder_cyfrowy/Form1.cs
@@ -21,67 +21,86 @@ public void dekoduj(PictureBox ktory_pikczer, char cyfra)
         {   //przekazano do tej procedury nazwę Pictureboxa który właśnie ustawiamy (jeden na raz)
             //dodatkowo przekazuję sobie znak (cyferkę) i switchem ustawię właściwy obrazek
             
-            //ify docelowo zastąpię switchem
-            if (cyfra == '5')
-                ktory_pikczer.Image = Properties.Resources._5;
-            if (cyfra == '7')
-                ktory_pikczer.Image = Properties.Resources._7;
-
-            ktory_pikczer.Refresh();
-            ktory_pikczer.Visible = true;
-            }
-        private void button0_Click(object sender, EventArgs e)
-        {
-
-           //rzutowanie liczby do stringa (tablicy charów);
-            string numer = Convert.ToString(numericUpDown1.Value);
-            label1.Text = numer;
-
-            dekoduj(pictureBox2,'5');   //wywołanie funkcji i przekazanie jej nazwy PictureBoxa oraz cyferki jaką trzeba wyświetlić
-            dekoduj(pictureBox3, '7');
-            //dekoduj(pictureBox1, numer[0]);
-            
-            //tego switcha przenieść do funkcji dekoduj()
-            switch (numer[0])
+            switch (cyfra)
             {
                 case '1':
-                    pictureBox1.Image = Properties.Resources._1;
-                    pictureBox1.Refresh();
-                    pictureBox1.Visible = true;
+                    ktory_pikczer.Image = Properties.Resources._1;
+                    ktory_pikczer.Refresh();
+                    ktory_pikczer.Visible = true;
                     break;
 
                 case '2':
-                    pictureBox1.Image = Properties.Resources._2;
-                    pictureBox1.Refresh();
-                    pictureBox1.Visible = true;
+                    ktory_pikczer.Image = Properties.Resources._2;
+                    ktory_pikczer.Refresh();
+                    ktory_pikczer.Visible = true;
                     break;
 
                 case '3':
-                    pictureBox1.Image = Properties.Resources._3;
-                    pictureBox1.Refresh();
-                    pictureBox1.Visible = true;
+                    ktory_pikczer.Image = Properties.Resources._3;
+                    ktory_pikczer.Refresh();
+                    ktory_pikczer.Visible = true;
                     break;
 
                 case '4':
-                    pictureBox1.Image = Properties.Resources._4;
-                    pictureBox1.Refresh();
-                    pictureBox1.Visible = true;
+                    ktory_pikczer.Image = Properties.Resources._4;
+                    ktory_pikczer.Refresh();
+                    ktory_pikczer.Visible = true;
                     break;
 
                 case '5':
-                    pictureBox1.Image = Properties.Resources._5;
-                    pictureBox1.Refresh();
-                    pictureBox1.Visible = true;
+                    ktory_pikczer.Image = Properties.Resources._5;
+                    ktory_pikczer.Refresh();
+                    ktory_pikczer.Visible = true;
                     break;
 
                 case '6':
-                    pictureBox1.Image = Properties.Resources._6;
-                    pictureBox1.Refresh();
-                    pictureBox1.Visible = true;
+                    ktory_pikczer.Image = Properties.Resources._6;
+                    ktory_pikczer.Refresh();
+                    ktory_pikczer.Visible = true;
+                    break;
+
+                case '7':
+                    ktory_pikczer.Image = Properties.Resources._7;
+                    ktory_pikczer.Refresh();
+                    ktory_pikczer.Visible = true;
+                    break;
+
+                case '8':
+                    ktory_pikczer.Image = Properties.Resources._8;
+                    ktory_pikczer.Refresh();
+                    ktory_pikczer.Visible = true;
+                    break;
+
+                case '9':
+                    ktory_pikczer.Image = Properties.Resources._9;
+                    ktory_pikczer.Refresh();
+                    ktory_pikczer.Visible = true;
                     break;
 
             };
-            
+        }
+        private void button0_Click(object sender, EventArgs e)
+        {
+
+            string numer = Convert.ToString(numericUpDown1.Value);      //rzutowanie liczby do stringa (tablicy charów);
+            label1.Text = numer;
+
+            dekoduj(pictureBox1, numer[0]);
+            dekoduj(pictureBox2, numer[1]);   //wywołanie funkcji i przekazanie jej nazwy PictureBoxa oraz cyferki jaką trzeba wyświetlić
+            dekoduj(pictureBox3, numer[2]);
+
+            dekoduj(pictureBox4, numer[3]);
+            dekoduj(pictureBox5, numer[4]);
+            dekoduj(pictureBox6, numer[5]);
+
+            dekoduj(pictureBox7, numer[6]);
+            dekoduj(pictureBox8, numer[7]);
+            dekoduj(pictureBox9, numer[8]);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
