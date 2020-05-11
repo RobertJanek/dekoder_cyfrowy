b8084a5a849fa300dd28005663f43b879a3fe374
Robert Paszkowski <robert.paszkowski@wat.edu.pl>
2020-05-11 02:44:44 +0200

    macierz cyferek zamiast kodowania

--- dekoder_cyfrowy/Form1.Designer.cs
+++ dekoder_cyfrowy/Form1.Designer.cs
@@ -179,81 +179,81 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(19, 209);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(13, 13);
             this.label1.TabIndex = 15;
-            this.label1.Text = "label1";
+            this.label1.Text = "0";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(19, 174);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(13, 13);
             this.label2.TabIndex = 16;
-            this.label2.Text = "label2";
+            this.label2.Text = "0";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(19, 138);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(13, 13);
             this.label3.TabIndex = 17;
-            this.label3.Text = "label3";
+            this.label3.Text = "0";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(61, 138);
+            this.label4.Location = new System.Drawing.Point(65, 209);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.Size = new System.Drawing.Size(13, 13);
             this.label4.TabIndex = 18;
-            this.label4.Text = "label4";
+            this.label4.Text = "0";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(64, 174);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.Size = new System.Drawing.Size(13, 13);
             this.label5.TabIndex = 19;
-            this.label5.Text = "label5";
+            this.label5.Text = "0";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(64, 209);
+            this.label6.Location = new System.Drawing.Point(64, 138);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.Size = new System.Drawing.Size(13, 13);
             this.label6.TabIndex = 20;
-            this.label6.Text = "label6";
+            this.label6.Text = "0";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(103, 138);
+            this.label7.Location = new System.Drawing.Point(106, 209);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.Size = new System.Drawing.Size(13, 13);
             this.label7.TabIndex = 21;
-            this.label7.Text = "label7";
+            this.label7.Text = "0";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
             this.label8.Location = new System.Drawing.Point(106, 173);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.Size = new System.Drawing.Size(13, 13);
             this.label8.TabIndex = 22;
-            this.label8.Text = "label8";
+            this.label8.Text = "0";
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(106, 208);
+            this.label9.Location = new System.Drawing.Point(106, 138);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.Size = new System.Drawing.Size(13, 13);
             this.label9.TabIndex = 23;
-            this.label9.Text = "label9";
+            this.label9.Text = "0";
             // 
             // Form1
             // 
